b6494eb4b9aa5492b32e99fa5647968a422875d3
Tayo Ogunbiyi <eyitayoogunbiyi@gmail.com>
2025-11-17 08:53:35 +0000

    Basic sampling loop

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,18 @@ mod model;
 mod sampler;
 
 use sampler::{PlatformSampler as Sampler, Sampler as _};
+use std::thread;
+use std::time::Duration;
 
+const SAMPLE_INTERVAL_SECS: u64 = 2;
 fn main() {
     let mut sampler = Sampler::new();
-    match sampler.sample() {
-        Ok(raw_sample) => println!("Sample collected: {:?}", raw_sample),
-        Err(e) => eprintln!("Error sampling: {:?}", e),
+
+    loop {
+        match sampler.sample() {
+            Ok(raw_sample) => println!("Sample collected: {:?}", raw_sample),
+            Err(e) => eprintln!("Error sampling: {:?}", e),
+        }
+        thread::sleep(Duration::from_secs(SAMPLE_INTERVAL_SECS));
     }
 }
